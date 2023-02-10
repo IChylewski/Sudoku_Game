9d8fb883308c24bdb47780b324fa939213eb4fd6
Irek Chylewski <IrekC@forrit.com>
2023-02-10 10:19:11 +0000

    Menu Implementation

---
+++ Menu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Sudoku_Final_Project;
+
+namespace Sudoku_Final_Project
+{
+    internal class Menu
+    {
+        SudokuGenerator generator = new SudokuGenerator();
+        public void DisplayMenu()
+        {
+            int menuChoice = 0;
+
+            while(menuChoice != 3)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Start Game");
+                Console.WriteLine("2. Game History");
+                Console.WriteLine("3. Exit Game");
+                Console.WriteLine("\nPlease enter number and press enter");
+                
+                if(!int.TryParse(Console.ReadLine(), out menuChoice))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter integer value!\n\n");
+                }
+
+                switch(menuChoice)
+                {
+                    case 1:
+                        StartGame();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter number 1-3");
+                        break;
+                }
+
+            }
+        }
+        public void StartGame()
+        {
+            int menuChoice = 0;
+            while (menuChoice != 5)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Easy");
+                Console.WriteLine("2. Medium");
+                Console.WriteLine("3. Hard");
+                Console.WriteLine("4. All Visible");
+                Console.WriteLine("5. Go To Menu");
+                Console.WriteLine("\nPlease enter number and press enter");
+
+                if (!int.TryParse(Console.ReadLine(), out menuChoice))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter integer value!\n\n");
+                }
+
+                switch(menuChoice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        generator.DrawSudoku(generator.GenerateSudoku(""));
+                        break;
+                    case 5:
+                        DisplayMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter number 1-5");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ SudokuGenerator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Final_Project
+{
+    internal class SudokuGenerator
+    {
+        public int[,] GenerateSudoku(string difficulty)
+        {
+            Random rnd = new Random();
+            int[,] board = new int[9, 9];
+            int[] numbers = Enumerable.Range(1, 9).ToArray();
+            int numbersToHide = SetDifficultyLevel(difficulty);
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int[] possibleNumbers = numbers.Where(x => IsValid(board, i, j, x)).ToArray();
+
+                    if (possibleNumbers.Length == 0)
+                    {
+                        i = 0;
+                        j = -1;
+                        board = new int[9, 9];
+                        continue;
+                    }
+                    int randomIndex = rnd.Next(possibleNumbers.Length);
+                    board[i, j] = possibleNumbers[randomIndex];
+                }
+            }
+            return board;
+        }
+
+        private bool IsValid(int[,] board, int row, int col, int num)
+        {
+
+            // Check if row or column contains number
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[row, i] == num || board[i, col] == num)
+                {
+                    return false;
+                }
+            }
+
+            // Set 3x3 box range to check
+            int startRow = (row / 3) * 3;
+            int startColumn = (col / 3) * 3;
+
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startColumn; j < startColumn + 3; j++)
+                {
+                    if (board[i, j] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+        public void DrawSudoku(int[,] board)
+        {
+            Console.Clear();
+            Console.WriteLine("    1 2 3 4 5 6 7 8 9\n");
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write((i+1) + "  ");
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j % 3 == 0)
+                    {
+                        Console.Write("|");
+                    }
+
+                    if (j == 1 || j == 4 || j == 7)
+                    {
+                        Console.Write(" " + board[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(board[i, j]);
+                    }
+
+                    if (j == 8)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+                if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("    -------------------");
+                }
+            }
+            PlaySudoku();
+        }
+        private int SetDifficultyLevel(string difficulty)
+        {
+            int numbersToHide;
+
+            switch(difficulty)
+            {
+                case "Easy":
+                    numbersToHide = 10;
+                    break;
+                case "Medium":
+                    numbersToHide = 20;
+                    break;
+                case "Hard":
+                    numbersToHide = 30;
+                    break;
+                default:
+                    numbersToHide = 0;
+                    break;
+            }
+            return numbersToHide;
+        }
+        private void PlaySudoku()
+        {
+            Console.WriteLine("\nFill cell using [x,y] = number                                                       Enter exit to go back to menu");
+            Console.ReadLine();
+        }
+    }
+}
